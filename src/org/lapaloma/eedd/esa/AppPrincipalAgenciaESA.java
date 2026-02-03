e216194ce1c9bebb1352f2f62353e7fa1eb46649
Agustin Martinez <ama302@educa.madrid.org>
2026-02-03 21:54:27 +0100

    Agregue mis iniciales al Main

--- src/org/lapaloma/eedd/esa/AppPrincipalAgenciaESA.java
+++ src/org/lapaloma/eedd/esa/AppPrincipalAgenciaESA.java
@@ -23,7 +23,7 @@ public class AppPrincipalAgenciaESA {
 
         ICuerpoCelesteDAO interfazAlumno= null;
         int idCuerpo= 2;  // Se usará para buscar el cuerpo Celeste que tiene como identificador 2
-        System.out.println("Cuerpo celeste con identificador: " + idCuerpo + " [realizado por Isidoro Nevares Martín ]");
+        System.out.println("Cuerpo celeste con identificador: " + idCuerpo + " [realizado por ama302 ]");
 
         interfazAlumno = new CuerpoCelesteDaoArray();
         // Consultar de Cuerpo Celeste por ID
